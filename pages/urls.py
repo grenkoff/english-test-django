c1733c45d3b1d6b72d25f39e380d7e1ef73d3f90
Alexander Grenkov <a.grenkov@gmail.com>
2024-06-14 04:07:51 +0500

    add: logout functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import HomePageView, QuizPageView
+from .views import (
+    HomePageView,
+    QuizPageView,
+    custom_logout_view,
+)
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
+    path("logout/", custom_logout_view, name='logout'),
     path("quiz/", QuizPageView.as_view(), name="quiz"),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView, ListView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
 from .models import Quiz
+
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -8,3 +12,8 @@ class HomePageView(TemplateView):
 class QuizPageView(ListView):
     model = Quiz
     template_name = "quiz.html"
+
+
+def custom_logout_view(request):
+    logout(request)
+    return redirect("home")
