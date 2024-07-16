b99792a12164701b5d2cb01ac853a615b4e1566c
Alexander Grenkov <a.grenkov@gmail.com>
2024-07-16 22:10:57 +0500

    add: registration functionalities

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import SignUpView
+from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("", include("django.contrib.auth.urls")),
+    path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
 ]
